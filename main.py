3e40d0ddf6705684cb0ce54c66344d7649394a01
milena-kos <75163549+milena-kos@users.noreply.github.com>
2022-03-27 11:55:11 +0300

    just a little test u know

--- main.py
+++ main.py
@@ -1406,7 +1406,7 @@ async def on_message(message):
                 if int(msg_author) == 553093932012011520:
                     embedVar = discord.Embed(title="KK dude, updating!", color=0x00FF00)
                     await message.reply(embed=add_ad(embedVar))
-                    result = str(subprocess.getoutput(["git", "pull"]))
+                    result = str(subprocess.getoutput(["git pull"]))
                     print(result)
                     await message.reply(result)
                     sys.stdout.flush()
