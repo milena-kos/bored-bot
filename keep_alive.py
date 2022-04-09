bf48da927a78b09d8853c882245c79cfeae02376
milena-kos <75163549+milena-kos@users.noreply.github.com>
2022-04-09 21:24:48 +0300

    add i hate myself back

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, current_app
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def main():
-  return "Bot is running!"
+    return current_app.send_static_file('main.html')
 
 def run():
   app.run(host="0.0.0.0", port=8080)
